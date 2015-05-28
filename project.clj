fdf12bf4ee7473703b99e1baab18c70ea252d4f0
Janne Haarni <janne.haarni@elisa.fi>
2015-05-28 20:17:10 +0300

    bump everything

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(def camel-version "2.11.0")
+(def camel-version "2.15.2")
 
-(defproject clj-camel-camelarius "0.3.2" 
+(defproject camelarius "0.4.0" 
   :description "Apache Camel DSL in Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.apache.camel/camel-core ~camel-version]
                  [org.clojure/tools.logging "0.2.6"]]
   :source-paths ["src"]
 
-  :profiles {:dev {:dependencies [[midje "1.5.1"]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [midje-junit-formatter "0.1.0-SNAPSHOT"]]
-                   :plugins [[lein-midje "3.0.0"]]}})
+                   :plugins [[lein-midje "3.1.3"]]}})
